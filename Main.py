7bc27bd943a7dd6392bc336884a54e4c103d2708
Amanda Giarla <amanda@sonic.net>
2016-02-08 09:24:34 -0800

    Added the mail file model

---
+++ Main.py
@@ -0,0 +1,11 @@
+#__author__ == 'amanda'
+
+def main():
+    print("The Main function started", end='\n')
+
+
+# here is the sample code of the start main syntax.
+# notice the : after the conditional.
+# notice that the main is defined above.
+if __name__ == "__main__":
+    main()
